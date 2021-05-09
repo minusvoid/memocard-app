ced2a00c6b6b88532bcec17df75c086c1b3b4ade
minusvoid <zilchminusvoid@gmail.com>
2021-05-09 13:29:46 +0900

    add_cardinput.tsx

---
+++ src/components/cardinput.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { Card } from './Types';
+
+type Props = {
+	cards: Card[]
+	addCards: React.Dispatch<React.SetStateAction<Card[]>>
+}
+
+const CardInput: React.FC<Props> = ({ addCards, cards}) => {
+	const [ inputTitle, setInputTitle ] = useState<string>('')
+	const [ count, setCount ] = useState<number>( cards.length +1 )
+
+	const handleInputChange = ( e: React.ChangeEvent<HTMLInputElement>) => {
+		setInputTitle( e.target.value )
+	}
+
+	const handleSubmit = () => {
+		setCount( count +1 )
+
+		const newCard: Card ={
+			id: count,
+			title: inputTitle,
+			pinned: false,
+		}
+
+		setCards([ newCard, ... cards ])
+		setInputTitle('')
+	}
+
+	return (
+		<div>
+			<div className="imputForm">
+				<div className="inner">
+					<input
+						type="text"
+						className="input"
+						value={inputTitle}
+						onChange={handleInputChange}
+					/>
+					<button onClick={handleSubmit} className="btn is-primary">
+						Add
+					</button>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default CardInput;
